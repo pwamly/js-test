0f95159969477a5c525963cef0adde1a371d58be
pwamly <pwamly2016@gmail.com>
2023-01-03 23:59:05 +0300

    object revisions

---
+++ rev.js
@@ -0,0 +1,20 @@
+// 1.cunstructor objects
+// let me = new Object();
+// console.log(me);
+
+// 2.literal objects
+// let lme = {};
+// console.log(me);
+
+// 3.computed properties in literal objects
+// let prop1=prompt("enter prop name",'');
+// let obj = {
+// [prop1]:'pwamly'
+// }
+// // console.log(obj);
+
+// 4. square bracket
+// let prop1=prompt("enter prop name",'')
+// let obj={}
+// obj[prop1?'wife':'friend']='pwamly';
+// console.log(obj);
